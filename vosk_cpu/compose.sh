cfd75a476a59b20f9969bd13c4dfb6870a280197
alex <format37@gmail.com>
2023-01-31 15:50:01 +0400

    modified:   ../compose.sh 	modified:   ../docker-compose.yml 	deleted:    ../server/Dockerfile 	deleted:    ../server/server.py

--- vosk_cpu/compose.sh
+++ vosk_cpu/compose.sh
@@ -1,3 +1,3 @@
-# Recreate
-sudo docker-compose up --build -d --force-recreate
+# Recreate and rebuild
+sudo docker-compose up --build -d
 # sudo docker run --name kaldi_cpu_0 -e VOSK_SERVER_PORT=2800 -d -p 2800:2800 alphacep/kaldi-ru:latest
