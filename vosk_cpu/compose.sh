f6d49b7a055bb2e9f97174b29c4cc14bc2f2b2bd
alex <format37@gmail.com>
2023-01-31 15:33:11 +0400

    modified:   ../compose.sh

--- vosk_cpu/compose.sh
+++ vosk_cpu/compose.sh
@@ -1,2 +1,3 @@
-sudo docker-compose up --build -d
+# Recreate
+sudo docker-compose up --build -d --force-recreate
 # sudo docker run --name kaldi_cpu_0 -e VOSK_SERVER_PORT=2800 -d -p 2800:2800 alphacep/kaldi-ru:latest
