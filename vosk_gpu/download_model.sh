94364edc3f1ddedd3016468f15046fdfb0ccb301
Alex <format37@gmail.com>
2023-10-03 15:28:14 +0400

    modified:   ../vosk_gpu/Dockerfile 	modified:   ../vosk_gpu/download_model.sh 	new file:   openai_client.py

--- vosk_gpu/download_model.sh
+++ vosk_gpu/download_model.sh
@@ -1,1 +1,2 @@
-wget https://alphacephei.com/vosk/models/vosk-model-ru-0.42.zip
+# wget https://alphacephei.com/vosk/models/vosk-model-ru-0.42.zip
+wget https://alphacephei.com/vosk/models/vosk-model-en-us-0.42-gigaspeech.zip
