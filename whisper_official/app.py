58d4db44c04f25b7a484c8baf612a8ef932ebeda
Alex <format37@gmail.com>
2023-11-07 01:26:04 +0400

    new file:   Dockerfile 	new file:   app.py 	new file:   build.sh 	new file:   requirements.txt 	new file:   ru.mp3 	new file:   run.sh

---
+++ whisper_official/app.py
@@ -0,0 +1,11 @@
+# Short
+import whisper
+
+model = whisper.load_model("large", download_root='/app/cache')
+result = model.transcribe(
+    "ru.mp3",
+    language="ru",
+    temperature=0.8,
+    prompt="Звонок в сервисный центр по ремонту бытовой техники. Телефонные гудки не учитывать."
+    )
+print(result["text"])
---
+++ whisper_official/run.sh
@@ -0,0 +1,4 @@
+# sudo docker run --gpus '"device=0"' -p 2700:2700 -t whisper
+# Create folder ./cache if not exist
+mkdir -p cache
+sudo docker run --gpus '"device=0"' -p 2700:2700 -v $(pwd)/cache:/app/cache -t whisper_official
